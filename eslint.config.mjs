d61b8fbdcb6ce4377e268e6a7ea4400229d4eac6
edgaras.spiridonovas <edgaras.spiridonovas@fltechnics.com>
2024-09-26 16:14:13 +0300

    Enhance ESLint configuration with detailed rule comments
    Added comments for existing ESLint rules to improve clarity and documentation. Introduced new rules to limit function complexity, enforce callback error handling, and disallow potentially hazardous JavaScript constructs.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,22 +7,26 @@ export default [
   pluginJs.configs.recommended,
   {
     rules: {
-      'no-console': 'warn',
-      eqeqeq: ['error', 'always'],
-      semi: ['error', 'always'],
-      quotes: ['error', 'single'],
-      indent: ['error', 2],
-      curly: 'error',
-      'no-unused-vars': ['warn'],
-      'consistent-return': 'error',
-      'no-undef': 'error',
-      'arrow-parens': ['error', 'always'],
-      'object-curly-spacing': ['error', 'always'],
-      'array-bracket-spacing': ['error', 'never'],
-      'no-multiple-empty-lines': ['error', { max: 1 }],
-      'max-len': ['error', { code: 120 }],
-      camelcase: 'error',
-      'prefer-const': 'error',
+      'no-console': 'warn', // Warn for console logs
+      'eqeqeq': ['error', 'always'], // Always use === and !==
+      'semi': ['error', 'always'], // Enforce semicolons
+      'quotes': ['error', 'single'], // Enforce single quotes
+      'indent': ['error', 2], // Indent using 2 spaces
+      'curly': 'error', // Require curly braces for control statements
+      'no-unused-vars': ['warn'], // Warn on unused variables
+      'consistent-return': 'error', // Enforce consistent return statements
+      'no-undef': 'error', // Disallow the use of undeclared variables
+      'arrow-parens': ['error', 'always'], // Require parentheses around arrow function arguments
+      'object-curly-spacing': ['error', 'always'], // Require spacing inside curly braces
+      'array-bracket-spacing': ['error', 'never'], // Disallow spaces inside array brackets
+      'no-multiple-empty-lines': ['error', { max: 1 }], // Limit consecutive empty lines
+      'max-len': ['error', { code: 120 }], // Maximum line length
+      'camelcase': 'error', // Enforce camelCase naming convention
+      'prefer-const': 'error', // Prefer const for variables that are never reassigned
+      'complexity': ['warn', { max: 10 }], // Limit complexity of functions
+      'handle-callback-err': 'error', // Require error handling in callbacks
+      'no-eval': 'error', // Disallow eval()
+      'no-new-func': 'error', // Disallow new Function()
     },
   },
 ];
